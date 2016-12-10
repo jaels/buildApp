339e1d10c552b9c76dab773d5a0b5fe113a107b3
Yael Stein <yaelst@gmail.com>
2016-12-10 02:20:07 +0100

    heroku fix

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ var express = require('express'),
 app = express(),
 cookieParser = require('cookie-parser'),
 cookieSession = require('cookie-session'),
-// http = require('http').Server(app),
-
-    server = require('http').createServer(app),
-
+http = require('http').Server(app),
 
 // csrf = require('csurf'),
 // csrfProtection = csrf({cookie: true}),
@@ -26,19 +23,7 @@ app.use(cookieSession({
 //     return next();
 // });
 
-
-// var express = require('express'),
-//     app = express(),
-//     server = require('http').createServer(app),
-//     io = require('socket.io').listen(server),
-//
-// server.listen(process.env.PORT || 3000);
-//
-
-
-    var io = require('socket.io').listen(server);
-
-// var io = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 
 var users={};
@@ -290,9 +275,8 @@ app.get('/logout', function(req, res) {
     res.json({success: true});
 });
 
- // http.listen(process.env.PORT || 3000);
+ http.listen(process.env.PORT || 3000);
 
- server.listen(process.env.PORT || 3000);
 
 
 
